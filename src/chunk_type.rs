b2056ac0a47f978792eff90e406cf9e1f2986ed9
rdxdkr <franchi.davide95@gmail.com>
2022-05-08 20:27:29 +0200

    test_chunk_type_from_bytes red

--- src/chunk_type.rs
+++ src/chunk_type.rs
@@ -1,1 +1,30 @@
+use crate::Error;
+
 struct ChunkType {}
+
+impl ChunkType {
+    fn bytes(&self) -> [u8; 4] {
+        todo!()
+    }
+}
+
+impl TryFrom<[u8; 4]> for ChunkType {
+    type Error = Error;
+
+    fn try_from(value: [u8; 4]) -> Result<Self, Self::Error> {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn test_chunk_type_from_bytes() {
+        let expected = [82, 117, 83, 116];
+        let actual = ChunkType::try_from([82, 117, 83, 116]).unwrap();
+
+        assert_eq!(expected, actual.bytes());
+    }
+}
