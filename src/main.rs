0fdfb7c3c4bc1e54ba0ac8ab4386ecf46de42937
rdxdkr <franchi.davide95@gmail.com>
2022-05-08 20:12:36 +0200

    basic setup in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error, result};
+
+pub type Error = Box<dyn error::Error>;
+pub type Result<T> = result::Result<T, Error>;
+
+fn main() -> Result<()> {
+    todo!()
 }
