3151625464a159ea60a05dc83b99fb59822b5e6f
rdxdkr <franchi.davide95@gmail.com>
2022-05-11 16:20:47 +0200

    test_chunk_trait_impls red

--- src/chunk.rs
+++ src/chunk.rs
@@ -78,6 +78,12 @@ impl Chunk {
     }
 }
 
+impl Display for Chunk {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
 impl TryFrom<&[u8]> for Chunk {
     type Error = Error;
 
@@ -216,6 +222,24 @@ mod tests {
         let chunk = Chunk::try_from(chunk_data.as_ref());
 
         assert!(chunk.is_err());
+    }
+
+    #[test]
+    pub fn test_chunk_trait_impls() {
+        let data_length: u32 = 42;
+        let chunk_type = "RuSt".as_bytes();
+        let message_bytes = "This is where your secret message will be!".as_bytes();
+        let crc: u32 = 2882656334;
+        let chunk_data: Vec<u8> = data_length
+            .to_be_bytes()
+            .iter()
+            .chain(chunk_type.iter())
+            .chain(message_bytes.iter())
+            .chain(crc.to_be_bytes().iter())
+            .copied()
+            .collect();
+        let chunk: Chunk = TryFrom::try_from(chunk_data.as_ref()).unwrap();
+        let _chunk_string = format!("{}", chunk);
     }
 
     fn testing_chunk() -> Chunk {
