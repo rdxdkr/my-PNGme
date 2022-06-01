cb9495afe8822a0bb00f0303d904988a17060e27
rdxdkr <franchi.davide95@gmail.com>
2022-06-02 00:55:33 +0200

    simplified decode method

--- src/args.rs
+++ src/args.rs
@@ -133,11 +133,7 @@ impl EncodeArgs {
 
 impl DecodeArgs {
     pub fn decode(&self) -> Result<String> {
-        let mut file = File::open(&self.file_path)?;
-        let mut buffer = Vec::<u8>::new();
-
-        file.read_to_end(&mut buffer).unwrap();
-
+        let buffer = fs::read(&self.file_path)?;
         let png = Png::try_from(&buffer[..])?;
 
         match png.chunk_by_type(&self.chunk_type) {
