f403f47bad3aa9d96ce0993496c3c8ca65c73ffb
rdxdkr <franchi.davide95@gmail.com>
2022-06-15 14:11:45 +0200

    test_chunk_from_bytes_invalid_crc_size red

--- src/chunk.rs
+++ src/chunk.rs
@@ -246,6 +246,24 @@ mod tests {
     }
 
     #[test]
+    fn test_chunk_from_bytes_invalid_crc_size() {
+        let data_length: u32 = 42;
+        let chunk_type = "RuSt".as_bytes();
+        let message_bytes = "This is where your secret message will be!".as_bytes();
+        let crc: u8 = 0;
+        let chunk_data: Vec<u8> = data_length
+            .to_be_bytes()
+            .iter()
+            .chain(chunk_type.iter())
+            .chain(message_bytes.iter())
+            .chain(crc.to_be_bytes().iter())
+            .copied()
+            .collect();
+
+        assert!(Chunk::try_from(chunk_data.as_ref()).is_err());
+    }
+
+    #[test]
     fn test_chunk_from_bytes_invalid_crc_value() {
         let data_length: u32 = 42;
         let chunk_type = "RuSt".as_bytes();
